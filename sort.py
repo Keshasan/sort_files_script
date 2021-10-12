d85c37934950959b72240a7a38631d98acabab95
Keshasan <kesha3084@gmail.com>
2021-10-13 00:44:53 +0300

    fixed remarks

--- sort.py
+++ sort.py
@@ -4,9 +4,7 @@ import shutil
 import os
 import re
 
-folder_names = ['archives', 'video', 'audio', 'documents', 'images']
-
-sorting_dict = {
+SORTING_DICT = {
     'archives': ('ZIP', 'GZ', 'TAR'),
     'video': ('AVI', 'MP4', 'MOV', 'MKV'),
     'audio': ('MP3', 'OGG', 'WAV', 'AMR'),
@@ -14,50 +12,7 @@ sorting_dict = {
     'images': ('JPEG', 'PNG', 'JPG', 'SVG'),
 }
 
-
-def main():
-    folder_path = sys.argv[1]
-
-    if len(sys.argv) > 2:
-        print('Too much arguments.')
-        return
-
-    folder_path = Path(folder_path)
-    main_dir = folder_path
-    if not folder_path.exists():
-        print("Invalid path to folder. Restart script with correct 'path'.")
-        return
-
-    recursive_sort_directory(folder_path, main_dir)
-    remove_empty_folders(folder_path)
-    unpack_archives(folder_path)
-
-
-def recursive_sort_directory(path: Path, main_dir: Path):
-    '''
-         Recursive sort directory
-    '''
-    if path.is_dir() and path.name not in folder_names:
-        for element in path.iterdir():
-            recursive_sort_directory(element, main_dir)
-
-    else:
-        file_extension = path.name.split('.')[1]
-        file_name = path.name.split('.')[0]
-
-        for folder, extension in sorting_dict.items():
-            if file_extension.upper() in extension:
-                if main_dir.joinpath(folder).exists():
-                    shutil.move(path.as_posix(),
-                                main_dir.joinpath(folder).joinpath(normalize(file_name)+'.'+file_extension))
-                else:
-                    os.mkdir(main_dir.joinpath(folder))
-                    shutil.move(path.as_posix(),
-                                main_dir.joinpath(folder).joinpath(normalize(file_name)+'.'+file_extension))
-
-
-def normalize(name: str):
-    table_symbols = ('абвгґдеєжзиіїйклмнопрстуфхцчшщюяыэАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯЫЭьъЬЪ',
+TABLE_SYMBOLS = ('абвгґдеєжзиіїйклмнопрстуфхцчшщюяыэАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯЫЭьъЬЪ',
                      (
                          *(u'abvhgde'), 'ye', 'zh', *
                          (u'zyi'), 'yi', *(u'yklmnoprstuf'),
@@ -70,7 +25,40 @@ def normalize(name: str):
                          *(u'_' * 4)
                      )
                      )
-    map_cyr_to_latin = {ord(src): dest for src, dest in zip(*table_symbols)}
+
+
+def recursive_sort_directory(path: Path, main_dir: Path):
+    '''
+         Recursive sort directory
+    '''
+    if path.is_dir() and path.name not in SORTING_DICT.keys():
+        for element in path.iterdir():
+            recursive_sort_directory(element, main_dir)
+
+    else:
+        file_extension = path.name.split('.')[1]
+        file_name = path.name.split('.')[0]
+
+        for folder, extension in SORTING_DICT.items():
+            if file_extension.upper() in extension:
+                if main_dir.joinpath(folder).exists():
+                    shutil.move(
+                                path.as_posix(),
+                                main_dir.joinpath(folder).joinpath(normalize(file_name)+'.'+file_extension)
+                    )
+                else:
+                    os.mkdir(main_dir.joinpath(folder))
+                    shutil.move(
+                                path.as_posix(),
+                                main_dir.joinpath(folder).joinpath(normalize(file_name)+'.'+file_extension)
+                    )
+
+
+def normalize(name: str):
+    '''
+        Transliterate cyrrylic symbols to latin 
+    '''
+    map_cyr_to_latin = {ord(src): dest for src, dest in zip(*TABLE_SYMBOLS)}
     rx = re.compile(r"[^\w_]")
     return rx.sub('_', name.translate(map_cyr_to_latin))
 
@@ -79,17 +67,18 @@ def remove_empty_folders(path: Path):
     '''
         Recursively removes empty folders + renames folders using 'normalize function'
     '''
-    if path.is_dir() and path.name not in folder_names:
+    if path.is_dir() and path.name not in SORTING_DICT.keys():
 
         for element in path.iterdir():
             if element.is_dir():
-                shutil.move(element, element.parent.joinpath(
-                    normalize(normalize(element.name))))
-                element = element.parent.joinpath(
-                    normalize(normalize(element.name)))
+                shutil.move(
+                            element, 
+                            element.parent.joinpath(normalize(element.name))
+                )
+                element = element.parent.joinpath(normalize(element.name))
                 remove_empty_folders(element)
 
-        if len([element for element in path.iterdir()]) == 0:
+        if not list(path.iterdir()):
             shutil.rmtree(path)
 
 
@@ -109,4 +98,18 @@ def unpack_archives(path: Path):
 
 
 if __name__ == '__main__':
-    main()
+    folder_path = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        print('Too much arguments.')
+        sys.exit()
+
+    folder_path = Path(folder_path)
+    main_dir = folder_path
+    if not folder_path.exists():
+        print("Invalid path to folder. Restart script with correct 'path'.")
+        sys.exit()
+
+    recursive_sort_directory(folder_path, main_dir)
+    remove_empty_folders(folder_path)
+    unpack_archives(folder_path)
